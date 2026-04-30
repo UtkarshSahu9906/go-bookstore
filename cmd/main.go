fa95927dde38742aec7f8998e34cc65b95241902
UtkarshSahu9906 <UtkarshSahu9906@gmail.com>
2026-05-01 00:34:56 +0530

    fix: correct function names and improve book model methods

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,14 @@ import{
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/UtkarshSahu9906/go-bookstore/pkg/routes"
 	
+}
+
+func main(){
+	r:=mux.NewRouter()
+	routes.ResisterBookStoreRoutes(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",));
 }
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -17,3 +17,26 @@ func init(){
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Creata(&b)
+	return b
+}
+func GetAllBooks() []Book{
+	var Books []Book
+	db.find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book,*grom.DB){
+	var getBook Book
+	db:=db.ehare("ID=?",Id).Find(&getBook)
+	return $getBook,db
+
+}
+
+func DeleteBook(ID int64)Book{
+	var book Book
+	db.Whare("ID=?",ID).Delete(book)
+	return book
+}
